c3a58eb8f818e39af38cabc6fe775b1afdf11b95
u16suzu <u16suzu@gmail.com>
2013-09-18 02:23:11 +0900

    とりあえずできた

--- collectionViewSpike/ViewController.m
+++ collectionViewSpike/ViewController.m
@@ -19,44 +19,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    self.collection = [[UICollectionView alloc]
+                       initWithFrame:CGRectMake(0, 0, 320, 480)
+                       collectionViewLayout:[UICollectionViewFlowLayout new]];
     [self.collection registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"MY_CELL"];
     
     [self.collection registerClass:[UIView class] forSupplementaryViewOfKind:@"Hoge" withReuseIdentifier:@"EEE"];
     
-    self.collection = [[UICollectionView alloc]
-                       initWithFrame:CGRectMake(0, 0, 320, 100)
-                       collectionViewLayout:[UICollectionViewFlowLayout new]];
-
     
     self.collection.delegate = self;
     self.collection.dataSource = self;
     
     [self.view addSubview:self.collection];
+
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView
      numberOfItemsInSection:(NSInteger)section{    
-    return 3;
+    return 10;
 }
 
-- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    return 2;
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake(50, 50);
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
-    // UICollectionViewCell * cell = [UICollectionViewCell new];
     
-    UICollectionViewCell *cell =
-    [collectionView dequeueReusableCellWithReuseIdentifier:@"MY_CELL" forIndexPath:indexPath];
+    UICollectionViewCell *cell=[collectionView dequeueReusableCellWithReuseIdentifier:@"MY_CELL" forIndexPath:indexPath];
     
-    
-    
-    cell.backgroundColor = [UIColor blueColor];
-    UIImage *img = [UIImage imageNamed:@"a.gif"];
-    UIImageView *iv = [[UIImageView alloc]initWithImage:img];
-    
-    [cell.contentView addSubview:iv];
+    cell.backgroundColor=[UIColor greenColor];
     return cell;
 }
 
