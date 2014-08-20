5767d480f9709b13fd5db52bc4c766eb0efcad40
u16suzu <u16suzu@gmail.com>
2014-08-20 19:53:41 +0900

    Refactoring

--- collectionViewSpike/ViewController.m
+++ collectionViewSpike/ViewController.m
@@ -9,48 +9,61 @@
 #import "ViewController.h"
 
 @interface ViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
-
-@property UICollectionView *collection;
-
+@property (nonatomic) UICollectionView *collectionView;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    self.collection = [[UICollectionView alloc]
-                       initWithFrame:CGRectMake(0, 0, 320, 480)
-                       collectionViewLayout:[UICollectionViewFlowLayout new]];
-    [self.collection registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"MY_CELL"];
-    
-    [self.collection registerClass:[UIView class] forSupplementaryViewOfKind:@"Hoge" withReuseIdentifier:@"EEE"];
-    
-    
-    self.collection.delegate = self;
-    self.collection.dataSource = self;
-    
-    [self.view addSubview:self.collection];
-
+- (void)loadView{
+    [super loadView];
+    [self.view addSubview:self.collectionView];
 }
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+}
+
+- (void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    self.collectionView.frame = CGRectMake(0, 20, 320, 568);
+}
+
+#pragma mark - Views
+
+- (UICollectionView*)collectionView{
+    if(!_collectionView){
+        _collectionView = [[UICollectionView alloc]
+                           initWithFrame:CGRectZero
+                           collectionViewLayout:[UICollectionViewFlowLayout new]];
+        
+        [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"MY_CELL"];
+        [_collectionView registerClass:[UIView class] forSupplementaryViewOfKind:@"Hoge" withReuseIdentifier:@"EEE"];
+        
+        _collectionView.delegate = self;
+        _collectionView.dataSource = self;
+    }
+    return _collectionView;
+}
+
+#pragma mark - CollectionView delegate methods
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView
      numberOfItemsInSection:(NSInteger)section{    
     return 10;
 }
 
-- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
-{
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
     return CGSizeMake(50, 50);
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MY_CELL" forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor greenColor];
     
-    UICollectionViewCell *cell=[collectionView dequeueReusableCellWithReuseIdentifier:@"MY_CELL" forIndexPath:indexPath];
-    
-    cell.backgroundColor=[UIColor greenColor];
     return cell;
 }
+
+#pragma mark -
 
 - (void)didReceiveMemoryWarning
 {
